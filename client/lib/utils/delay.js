51fb5ffeb748eec4a8a3d4d04b67f3afd52c34f0
hhi9037@naver.com <hhi9037@naver.com>
2023-01-19 15:02:46 +0900

    1/19 ajax 완성

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -101,46 +101,6 @@ delayP().then((res)=>{
 
 
 
-// async (일반함수를 promise를 반환하는 함수로 만든다)
-// await (1.promise가 반환하는 result를 가져오기. 2.코드 실행 흐름 제어)
-async function delayA() {
-  return '완료'
-}
-
-let result = await delayA();
-
-console.log(result);
-
-
-
-async function 라면끓이기() {
-  
-  try {
-
-  
-  await delayP()
-  console.log('물넣기');
-
-  await delayP()
-  console.log('스프넣기');
-
-  await delayP()
-  console.log('면넣기');
-
-  await delayP()
-  console.log('계란넣기');
-
-  // throw new Error('계란 껍질이 들어가버렸다!');
-  await delayP()
-  console.log('그릇에담기');
-
-  }catch(err){
-    console.log(err);
-  }
-  
-}
-
-라면끓이기();
 
 
 
--- client/lib/utils/tiger.js
+++ client/lib/utils/tiger.js
@@ -1,107 +1,70 @@
-
-
-
 const defaultOptions = {
-  method: 'GET',
-  mode: 'cors',
-  body:null,
-  cache: 'no-cache',
-  credential: 'same-origin',
-  redirect:'follow',
-  refererPolicy:'no-referer',
-  headers:{
-    'Content-Type':'application/json; charset=UTF-8'
-  }
-}
+  method: "GET",
+  mode: "cors",
+  body: null,
+  cache: "no-cache",
+  credential: "same-origin",
+  redirect: "follow",
+  referrerPolicy: "no-referrer",
+  headers: {
+    "Content-Type": "application/json; charset=UTF-8",
+  },
+};
 
-
-
-export const tiger = async (options = {}) =>{
-
-  const {url, ...restOptions} = {
+export const tiger = async (options = {}) => {
+  const { url, ...restOptions } = {
     ...defaultOptions,
     ...options,
-    headers: {...defaultOptions.headers, ...options.headers}
+    headers: { ...defaultOptions.headers, ...options.headers },
+  };
+
+  // let response = await fetch(url, restOptions);
+
+  let response = await fetch("https://jsonplaceholder.typicode.com/users", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  // 성공 -> response.ok
+  if (response.ok) {
+    response.data = await response.json();
   }
 
-
-  let response = await fetch(url,restOptions)
-
-  if(response.ok){
-    response.data = await response.json()
-  }
+  /*   response.then((res) => {
+    console.log(res);
+  }) */
 
   // console.log(response);
-
   return response;
-}
+};
+tiger.get = async (url, options) => {
+  return tiger({ url, ...options });
+};
 
-
-
-tiger.get = (url,options) => {
-  tiger({
+tiger.post = (url, body, options) => {
+  return tiger({
+    method: "POST",
     url,
-    ...options
-  })
-}
+    body: JSON.stringify(body),
+    ...options,
+  });
+};
 
-tiger.post = (url,body,options) =>{
-  tiger({
-    method:'POST',
+tiger.put = (url, body, options) => {
+  return tiger({
+    method: "PUT",
     url,
-    body:JSON.stringify(body),
-    ...options
-  })
-}
+    body: JSON.stringify(body),
+    ...options,
+  });
+};
 
-tiger.put = (url,body,options) =>{
-  tiger({
-    method:'PUT',
+tiger.delet = (url, options) => {
+  return tiger({
+    method: "DELETE",
     url,
-    body:JSON.stringify(body),
-    ...options
-  })
-}
-
-tiger.delete = (url,options) =>{
-  tiger({
-    method:'DELETE',
-    url,
-    ...options
-  })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    ...options,
+  });
+};
